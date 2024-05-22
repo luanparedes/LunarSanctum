551161a4e9e6ee372b474363a579a4ffa6605137
Luan Paredes <luan.developer.ti@gmail.com>
2024-05-22 01:47:19 -0300

    LS-0001: Initial commit

---
+++ Source/LunarSanctum/LunarSanctum.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LunarSanctum.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LunarSanctum, "LunarSanctum" );
